adf2c3bdd362d4c12b6c61102dbb81a44d44b47d
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-02-17 09:49:38 +0100

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,6 @@ angular.module('starter.services', [])
         getPicture: function(options) {
             var q = $q.defer();
             navigator.camera.getPicture(function(result) {
-                // Do any magic you need
                 q.resolve(result);
             }, function(err) {
                 q.reject(err);
