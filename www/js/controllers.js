fed453809305eccbf871d4e21ae1cd6ea617fceb
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-06 11:48:32 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,24 +45,6 @@ angular.module('starter.controllers', [])
             saveToPhotoAlbum: true
         });
     }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
 
 
